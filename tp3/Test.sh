f2802dca48e4d748d0924e3cf189f9091ed12c37
maxfoow <maxfoow@gmail.com>
2015-04-09 13:32:45 -0400

    correction 1

---
+++ tp3/Test.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+
+# Pour toujours faire les tests à partir d'un disque identique à l'original
+echo "Je copie le fichier DisqueVirtuel.dat.orig vers DisqueVirtuel.dat" 
+cp DisqueVirtuel.dat.orig DisqueVirtuel.dat
+
+echo
+echo "--------------------------------------------------------------------"
+echo "                     montrer le contenu du disque"
+echo "--------------------------------------------------------------------"
+./ufs ls /
+./ufs ls /doc
+./ufs ls /doc/tmp
+./ufs ls /doc/tmp/subtmp
+./ufs ls /rep
+./ufs ls /Bonjour
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "Tester les cas ou ls est fait sur un repertoire non-existant ou un fichier ordinaire"
+echo "--------------------------------------------------------------------"
+./ufs ls /mauvais
+./ufs ls /b.txt
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "Maintenant on verifie que les bons b.txt sont accédés"
+echo "Les numéros d'i-nodes doivent être différents"
+echo "--------------------------------------------------------------------"
+./ufs stat /doc/tmp/subtmp/b.txt 
+./ufs stat /b.txt 
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "    test de lecture d'un repertoire, fichier inexistant ou vide"
+echo "--------------------------------------------------------------------"
+./ufs read /rep 0 10
+./ufs read /toto.txt 0 10
+./ufs read /b.txt 0 10
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "                  test d'ecriture de 40 caracteres"
+echo "--------------------------------------------------------------------"
+./ufs write /b.txt "1234567890ABCDEFGHIJ1234567890ABCDEFGHIJ" 0 
+./ufs stat /b.txt
+./ufs blockfree
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "                          tests de lecture"
+echo "--------------------------------------------------------------------"
+
+./ufs read /b.txt 0 30
+./ufs read /b.txt 0 20
+./ufs read /b.txt 0 10
+./ufs read /b.txt 10 30
+./ufs read /b.txt 10 5
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "      test d'ecriture de 1 caracteres en milieu de fichier"
+echo "--------------------------------------------------------------------"
+./ufs write /b.txt "-" 14 
+./ufs stat /b.txt
+./ufs blockfree  
+./ufs read /b.txt 0 20
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "test d'ecriture de 1 caracteres, mais trop loin"
+echo "--------------------------------------------------------------------"
+./ufs write /b.txt "X" 41 
+./ufs read /b.txt 0 50
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "   test d'ecriture exactement après le dernier caractère du fichier"
+echo "--------------------------------------------------------------------"
+./ufs write /b.txt "+" 40 
+./ufs stat /b.txt
+./ufs read /b.txt 0 50
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "test d'ecriture augmentant la taille du fichier, mais sans saisie de nouveau bloc"
+echo "--------------------------------------------------------------------"
+./ufs write /b.txt "abcdefghij" 40 
+./ufs stat /b.txt
+./ufs blockfree; N_FREEBLOCK=$?;  
+./ufs read /b.txt 0 60
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "  test d'ecriture qui doit provoquer la saisie de 2 nouveaux blocs"
+echo "--------------------------------------------------------------------"
+./ufs write /b.txt "abcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJabcdefghij1234567890ABCDEFGHIJ" 0 
+./ufs stat /b.txt
+let "N_FREEBLOCK=$N_FREEBLOCK-2"
+echo -e "\nDoit afficher $N_FREEBLOCK blocs de libre:"
+./ufs blockfree  
+
+echo
+echo
+echo "-----------------------------------------------------------------------"
+echo "  test de lecture dans le fichier plus gros, qui chevauche deux blocs"
+echo "-----------------------------------------------------------------------"
+./ufs read /b.txt 0 600
+./ufs read /b.txt 500 30
+./ufs read /b.txt 500 100
+
+exit
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "                    Tester la commande hardlink"
+echo "--------------------------------------------------------------------"
+echo "Le nombre de blocs libre ne doit pas changer"
+./ufs blockfree; N_FREEBLOCK=$?;
+echo -e "\nDoit réussir:"
+./ufs hardlink /b.txt /hlnb.txt
+echo -e "\nDoit échouer avec -2, car hlnb.txt existe déjà:"
+./ufs hardlink /b.txt /hlnb.txt
+echo -e "\nDoit afficher $N_FREEBLOCK blocs de libre:"
+./ufs blockfree
+echo -e "\nDoit afficher les mêmes numéros d'i-node pour /b.txt et /hlnb.txt:"
+./ufs ls /
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "                    Tester la commande unlink"
+echo "--------------------------------------------------------------------"
+./ufs unlink /b.txt
+./ufs ls /
+echo -e "\nDoit afficher $N_FREEBLOCK blocs de libre, car l'inode est toujours détenu par hlnb.txt:"
+./ufs blockfree
+./ufs unlink /hlnb.txt
+./ufs ls /
+let "N_FREEBLOCK=$N_FREEBLOCK+1"
+echo -e "\nDoit afficher $N_FREEBLOCK blocs de libre, car l'inode a été libéré:"
+./ufs blockfree
+echo -e "\nDoit échouer avec -1, car /b.txt n'existe plus:"
+./ufs unlink /b.txt
+echo -e "\nDoit échouer avec -1, car /doc/tmp/b.txt n'existe pas:"
+./ufs unlink /doc/tmp/b.txt 
+./ufs unlink /doc/tmp/subtmp/b.txt 
+./ufs ls /doc/tmp/subtmp
+echo -e "\nDoit échouer avec -2, car /doc est un répertoire:"
+./ufs unlink /doc
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "                    Tester la commande rmdir"
+echo "--------------------------------------------------------------------"
+./ufs blockfree; N_FREEBLOCK=$?;
+./ufs rmdir /rep
+./ufs ls /
+let "N_FREEBLOCK=$N_FREEBLOCK+1"
+echo -e "\nDoit afficher $N_FREEBLOCK blocs de libre, car le fichier répertoire a été libéré:"
+./ufs blockfree
+echo -e "\nDoit échouer avec -3, car /doc n'est pas vide:."
+./ufs rmdir /doc
+./ufs ls /
+echo -e "\nDoit échouer avec -3, car /doc/tmp n'est pas vide:"
+./ufs rmdir /doc/tmp
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "              Tester la création d'un fichier vide"
+echo "--------------------------------------------------------------------"
+./ufs create /Doge.wow
+./ufs ls /
+./ufs create /doc/tmp/new.txt 
+./ufs ls /
+./ufs ls /doc/tmp
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "          Tester la fonction rename sur fichier ordinaire"
+echo "--------------------------------------------------------------------"
+./ufs rename /Bonjour/LesAmis.txt /Bonjour/OncleG.txt
+./ufs ls /Bonjour
+./ufs rename /Bonjour/OncleG.txt /DansRoot.txt
+./ufs ls /Bonjour
+./ufs ls /
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "                Tester la création d'un répertoire"
+echo "--------------------------------------------------------------------"
+./ufs blockfree; N_FREEBLOCK=$?;
+./ufs ls /Bonjour
+./ufs mkdir /Bonjour/newdir
+let "N_FREEBLOCK=$N_FREEBLOCK-1"
+echo -e "\nDoit afficher $N_FREEBLOCK blocs de libre, car le fichier répertoire a utilisé un bloc:"
+./ufs blockfree
+echo -e "\nOn vérifie que le nombre de lien nlink pour /Bonjour augmente de 1, à cause du sous-répertoire newdir:"
+./ufs ls /Bonjour
+./ufs ls /
+
+echo
+echo
+echo "--------------------------------------------------------------------"
+echo "            Tester la fonction rename sur répertoire"
+echo "--------------------------------------------------------------------"
+./ufs ls /Bonjour
+./ufs ls /doc
+./ufs rename /doc/tmp /Bonjour/tmpmv
+echo -e "\nOn vérifie que le nombre de lien pour /Bonjour augmente de 1 et qu'il diminue de 1 pour /doc:"
+./ufs ls /
+echo -e "\nOn vérifie que le sous-réperoire tmpmv contient encore subtmp et new.txt:"
+./ufs ls /Bonjour/tmpmv
+echo -e "\nOn vérifie que le nombre de lien vers ce même répertoire n'augmente pas si on répète l'opération:"
+./ufs rename /Bonjour/tmpmv /Bonjour/tmpmv2
+./ufs rename /Bonjour/tmpmv2 /Bonjour/tmpmv3
+./ufs ls /Bonjour
+
+
--- tp3/libUFS/sources/io_operation.c
+++ tp3/libUFS/sources/io_operation.c
@@ -7,185 +7,191 @@
 #include "inode_tools.h"
 
 static int		_find_block_by_offset(iNodeEntry *fileEntry,
-                                      int offset)
+					      int offset)
 {
-	if (offset > fileEntry->iNodeStat.st_size) {
-		offset = fileEntry->iNodeStat.st_size;
-	}
-	int			position = offset / BLOCK_SIZE;
+  if (offset > fileEntry->iNodeStat.st_size) {
+    offset = fileEntry->iNodeStat.st_size;
+  }
+  int			position = offset / BLOCK_SIZE;
 
-	if (position > N_BLOCK_PER_INODE) {
+  if (position > N_BLOCK_PER_INODE) {
 #if !defined(NDEBUG)
-		fprintf(stderr, "Function: %s: offset(%d) is out of range\n", __PRETTY_FUNCTION__, offset);
+    fprintf(stderr, "Function: %s: offset(%d) is out of range\n", __PRETTY_FUNCTION__, offset);
 #endif
-		return -1;
-	}
-	return position;
+    return -1;
+  }
+  return position;
 }
 
 static int		_file_controle(iNodeEntry *fileEntry,
-                               const char *pFilename,
-                               int offset)
+				       const char *pFilename,
+				       int offset)
 {
-	char		*path = strdup(pFilename);
+  char		*path = strdup(pFilename);
 
-	if (path == NULL) {
+  if (path == NULL) {
 #if defined(NDEBUG)
-		fprintf(stderr, "Function: %s: strdup failure", __PRETTY_FUNCTION__);
+    fprintf(stderr, "Function: %s: strdup failure\n", __PRETTY_FUNCTION__);
 #endif
-		return -1;
-	}
-	if (resolvePath(path, fileEntry) == -1) {
-		free(path);
+    return -1;
+  }
+  if (resolvePath(path, fileEntry) == -1) {
+    free(path);
 #if defined(NDEBUG)
-		fprintf(stderr, "Function: %s: resolvePath(%s) failure", __PRETTY_FUNCTION__, path);
+    fprintf(stderr, "Function: %s: resolvePath(%s) failure\n", __PRETTY_FUNCTION__, path);
 #endif
-		return -1;
-	}
-	free(path);
-	if (G_ISDIR(fileEntry->iNodeStat.st_mode)) {
+    return -1;
+  }
+  free(path);
+  if (G_ISDIR(fileEntry->iNodeStat.st_mode)) {
 #if defined(NDEBUG)
-		fprintf(stderr, "Function: %s: %s is a directory", __PRETTY_FUNCTION__, path);
+    fprintf(stderr, "Function: %s: %s is a directory\n", __PRETTY_FUNCTION__, path);
 #endif
-		return -2;
-	}
-	if (offset > fileEntry->iNodeStat.st_size) {
+    return -2;
+  }
+  if (offset > fileEntry->iNodeStat.st_size) {
 #if defined(NDEBUG)
-		fprintf(stderr, "Function: %s: offset: %d is out of range", __PRETTY_FUNCTION__, offset);
+    fprintf(stderr, "Function: %s: offset: %d is out of range\n", __PRETTY_FUNCTION__, offset);
 #endif
-		return -3;
-	}
-	return 0;
+    return -3;
+  }
+  return 0;
 }
 
 static int		_write_data(iNodeEntry *fileEntry,
-                            const char *buffer,
-                            int offset,
-                            int numbytes)
+				    const char *buffer,
+				    int offset,
+				    int numbytes)
 {
-	int			currentBlockPosition = -1;
-	int			numbytes_write = 0;
-	char		writeBuffer[BLOCK_SIZE];
-	char		isNewBlock = 0;
-	int			error;
+  int			currentBlockPosition = -1;
+  int			numbytes_write = 0;
+  char		writeBuffer[BLOCK_SIZE];
+  char		isNewBlock = 0;
+  int			error;
+  char			isOnlyRewrite = 1;
 
-	if (fileEntry->iNodeStat.st_blocks != 0 && (currentBlockPosition = _find_block_by_offset(fileEntry, offset)) == -1) {
-		return -1;
-	}
-	fileEntry->iNodeStat.st_size = offset;
-	while (numbytes_write != numbytes) {
-		isNewBlock = 0;
-		if (BLOCK_SIZE * fileEntry->iNodeStat.st_blocks == fileEntry->iNodeStat.st_size) {
-			if ((error = addBlock(fileEntry)) == -1) {
+  if (fileEntry->iNodeStat.st_blocks != 0 && (currentBlockPosition = _find_block_by_offset(fileEntry, offset)) == -1) {
+    return -1;
+  }
+  if (offset + numbytes >= fileEntry->iNodeStat.st_size) {
+    fileEntry->iNodeStat.st_size = offset;
+    isOnlyRewrite = 0;
+  }
+  while (numbytes_write != numbytes) {
+    isNewBlock = 0;
+    if (BLOCK_SIZE * fileEntry->iNodeStat.st_blocks == fileEntry->iNodeStat.st_size) {
+      if ((error = addBlock(fileEntry)) == -1) {
 #if !defined(NDEBUG)
-				fprintf(stderr, "Function: %s: cannot add block to current file\n", __PRETTY_FUNCTION__);
+	fprintf(stderr, "Function: %s: cannot add block to current file\n", __PRETTY_FUNCTION__);
 #endif
-				return error;
-			}
-			++currentBlockPosition;
-			isNewBlock = 1;
-		}
-		int		localOffset = offset % BLOCK_SIZE;
-		int		writeSize = numbytes - numbytes_write > BLOCK_SIZE ? BLOCK_SIZE - localOffset : numbytes - numbytes_write;
-		if (!isNewBlock) {
-			if (ReadBlock(fileEntry->Block[currentBlockPosition], writeBuffer) == -1) {
+	return error;
+      }
+      ++currentBlockPosition;
+      isNewBlock = 1;
+    }
+    int		localOffset = offset % BLOCK_SIZE;
+    int		writeSize = numbytes - numbytes_write > BLOCK_SIZE ? BLOCK_SIZE - localOffset : numbytes - numbytes_write;
+    if (!isNewBlock) {
+      if (ReadBlock(fileEntry->Block[currentBlockPosition], writeBuffer) == -1) {
 #if !defined(NDEBUG)
-				fprintf(stderr, "Function: %s: ReadBlock(%d) Failure\n", __PRETTY_FUNCTION__, fileEntry->Block[currentBlockPosition]);
+	fprintf(stderr, "Function: %s: ReadBlock(%d) Failure\n", __PRETTY_FUNCTION__, fileEntry->Block[currentBlockPosition]);
 #endif
-				return -1;
-			}
-		}
-		memcpy(writeBuffer + localOffset,
-		       buffer + numbytes_write,
-		       writeSize);
-		if (WriteBlock(fileEntry->Block[currentBlockPosition], writeBuffer) == -1) {
+	return -1;
+      }
+    }
+    memcpy(writeBuffer + localOffset,
+	   buffer + numbytes_write,
+	   writeSize);
+    if (WriteBlock(fileEntry->Block[currentBlockPosition], writeBuffer) == -1) {
 #if !defined(NDEBUG)
-			fprintf(stderr, "Function: %s: WriteBlock(%d) Failure\n", __PRETTY_FUNCTION__, fileEntry->Block[currentBlockPosition]);
+      fprintf(stderr, "Function: %s: WriteBlock(%d) Failure\n", __PRETTY_FUNCTION__, fileEntry->Block[currentBlockPosition]);
 #endif
-			return -1;
-		}
-		offset = 0;
-		numbytes_write += writeSize;
-		fileEntry->iNodeStat.st_size += writeSize;
-	}
-	if (saveINodeEntry(fileEntry) == -1) {
-		return -1;
-	}
-	return numbytes;
+      return -1;
+    }
+    offset = 0;
+    numbytes_write += writeSize;
+    if (!isOnlyRewrite)
+      fileEntry->iNodeStat.st_size += writeSize;
+  }
+  if (saveINodeEntry(fileEntry) == -1) {
+    return -1;
+  }
+  return numbytes;
 }
 
 int				_bd_write(const char *pFilename,
-                          const char *buffer,
-                          int offset,
-                          int numbytes)
+					  const char *buffer,
+					  int offset,
+					  int numbytes)
 {
-	iNodeEntry	fileEntry;
-	int			error;
-	if ((error = _file_controle(&fileEntry, pFilename, offset)) == -1) {
-		return error;
-	}
-	return _write_data(&fileEntry, buffer, offset, numbytes);
+  iNodeEntry	fileEntry;
+  int			error;
+  if ((error = _file_controle(&fileEntry, pFilename, offset)) != 0) {
+    return error;
+  }
+  return _write_data(&fileEntry, buffer, offset, numbytes);
 }
 
 static int		_read_data(iNodeEntry *fileEntry,
-                           char *buffer,
-                           int offset,
-                           int numbytes)
+				   char *buffer,
+				   int offset,
+				   int numbytes)
 {
-	char		readBuffer[BLOCK_SIZE];
-	int			currentBlockPosition;
-	int			numbytes_read = 0;
+  char		readBuffer[BLOCK_SIZE];
+  int			currentBlockPosition;
+  int			numbytes_read = 0;
 
-	while (numbytes_read != numbytes) {
-		if ((currentBlockPosition = _find_block_by_offset(fileEntry, offset)) == -1) {
-			return -1;
-		}
-		if (ReadBlock(fileEntry->Block[currentBlockPosition], readBuffer) == -1) {
+  while (numbytes_read != numbytes) {
+    if ((currentBlockPosition = _find_block_by_offset(fileEntry, offset)) == -1) {
+      return -1;
+    }
+    if (ReadBlock(fileEntry->Block[currentBlockPosition], readBuffer) == -1) {
 #if !defined(NDEBUG)
-			fprintf(stderr, "Function: %s: ReadBlock(%d) Failure\n", __PRETTY_FUNCTION__, fileEntry->Block[currentBlockPosition]);
+      fprintf(stderr, "Function: %s: ReadBlock(%d) Failure\n", __PRETTY_FUNCTION__, fileEntry->Block[currentBlockPosition]);
 #endif
-			return -1;
-		}
-		int		localOffset = offset % BLOCK_SIZE;
-		int		readSize = (numbytes - numbytes_read > BLOCK_SIZE ? BLOCK_SIZE - localOffset : numbytes - numbytes_read);
-		memcpy(buffer + numbytes_read,
-		       readBuffer + localOffset,
-		       readSize);
-		numbytes_read += readSize;
-		offset += readSize;
-	}
-	return numbytes_read;
+      return -1;
+    }
+    int		localOffset = offset % BLOCK_SIZE;
+    int		readSize = ((numbytes - numbytes_read) > BLOCK_SIZE - localOffset ? BLOCK_SIZE - localOffset :(numbytes - numbytes_read > BLOCK_SIZE ? BLOCK_SIZE - localOffset : numbytes - numbytes_read));
+    printf("%d %d %d %d %d\n", offset, localOffset, readSize, BLOCK_SIZE, numbytes - numbytes_read);
+    memcpy(buffer + numbytes_read,
+	   readBuffer + localOffset,
+	   readSize);
+    numbytes_read += readSize;
+    offset += readSize;
+  }
+  return numbytes_read;
 }
 
 int				_bd_read(const char *pFilename,
-                         char *buffer,
-                         int offset,
-                         int numbytes)
+					 char *buffer,
+					 int offset,
+					 int numbytes)
 {
-	iNodeEntry	fileEntry;
-	int			error;
-	if ((error = _file_controle(&fileEntry, pFilename, offset)) == -1) {
-		return error;
-	}
-	return _read_data(&fileEntry, buffer, offset, (offset + numbytes > fileEntry.iNodeStat.st_size ? fileEntry.iNodeStat.st_size - offset : numbytes));
+  iNodeEntry	fileEntry;
+  int			error;
+  if ((error = _file_controle(&fileEntry, pFilename, offset)) == -1) {
+    return error;
+  }
+  return _read_data(&fileEntry, buffer, offset, (offset + numbytes > fileEntry.iNodeStat.st_size ? fileEntry.iNodeStat.st_size - offset : numbytes));
 }
 
 int				_bd_truncate(const char *pFilename,
-                             int offset)
+					     int offset)
 {
-	iNodeEntry	fileEntry;
-	int			error;
-	if ((error = _file_controle(&fileEntry, pFilename, offset)) == -1) {
-		return error;
-	}
-	int numberOfReleaseBlock = (fileEntry.iNodeStat.st_size - offset) / BLOCK_SIZE;
-	printf("number : %d\n", numberOfReleaseBlock);
-	if (numberOfReleaseBlock != 0 && freeBlock(&fileEntry, numberOfReleaseBlock) == -1) {
-		return -1;
-	}
-	fileEntry.iNodeStat.st_size -= (fileEntry.iNodeStat.st_size - offset) - (numberOfReleaseBlock * BLOCK_SIZE);
-	if (saveINodeEntry(&fileEntry) == -1) {
-		return -1;
-	}
-	return 0;
+  iNodeEntry	fileEntry;
+  int			error;
+  if ((error = _file_controle(&fileEntry, pFilename, offset)) == -1) {
+    return error;
+  }
+  int numberOfReleaseBlock = (fileEntry.iNodeStat.st_size - offset) / BLOCK_SIZE;
+  printf("number : %d\n", numberOfReleaseBlock);
+  if (numberOfReleaseBlock != 0 && freeBlock(&fileEntry, numberOfReleaseBlock) == -1) {
+    return -1;
+  }
+  fileEntry.iNodeStat.st_size -= (fileEntry.iNodeStat.st_size - offset) - (numberOfReleaseBlock * BLOCK_SIZE);
+  if (saveINodeEntry(&fileEntry) == -1) {
+    return -1;
+  }
+  return 0;
 }
